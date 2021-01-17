d438321da9f53c0aa04c88e99aed74242a7453d8
jgabari <j.gabari.2017@alumnos.urjc.es>
2021-01-17 18:23:32 +0100

    estructuras de datos, lectura de xml y log

--- uaclient.py
+++ uaclient.py
@@ -8,50 +8,86 @@ import socket
 import sys
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+import time
 
 # Clase para procesar el xml
 class XMLHandler(ContentHandler):
     def __init__(self):
-        self.
+        self.content = {}
+        self.names = {'account': ['username', 'passwd'],
+                           'uaserver': ['ip', 'puerto'],
+                           'rtpaudio': ['puerto'],
+                           'regproxy': ['ip', 'puerto'],
+                           'log': ['path'],
+                           'audio': ['path']}
 
     def startElement(self, name, attrs):
-        if name ==
+        if name in list(self.names):
+            for attr in self.names[name]:
+                auxdict = {}
+                auxdict[attr] = attrs.get(attr, "")
+                self.content[name] = auxdict
 
-try:
-    CONFIG = sys.argv[1]
-    METHOD = sys.argv[2]
-except IndexError:
-    sys.exit("Usage: python3 uaclient.py config method option")
+    def get_tags(self):
+        return self.content
 
-SERVER_NAME = address.split('@')[0]
-SERVER_IP = address.split('@')[1].split(':')[0]
-SERVER_PORT = int(address.split('@')[1].split(':')[1])
-SDP = '\r\nv=0\r\no=robin@gotham.com 127.0.0.1\r\n'
-SDP += 's=misesion\r\nt=0\r\nm=audio 34543 RTP\r\n'
-content_length = 'Content-Length: ' + str(len(SDP)) + '\r\n'
-# Contenido que vamos a enviar
-if METHOD == 'INVITE':
-    LINE = 'INVITE sip:' + SERVER_NAME + '@' + SERVER_IP + ' SIP/2.0\r\n'
-    LINE += content_length + SDP
-elif METHOD == 'BYE':
-    LINE = 'BYE sip:' + SERVER_NAME + '@' + SERVER_IP + ' SIP/2.0\r\n'
+def writelog(data, log):
+    hora = time.strftime('%H', time.gmtime(time.time()))
+    minuto = time.strftime('%M', time.gmtime(time.time()))
+    segundo = time.strftime('%S', time.gmtime(time.time()))
+    ss = int(hora)*3600 + int(minuto)*60 + int(segundo)
+    fecha = time.strftime('%Y-%m-%d ', time.gmtime(time.time()))
+    fich = open(log, 'a')
+    fich.write(fecha + str(ss) + ' ' + data + '\r\n')
+    fich.close()
 
-ACK_LINE = 'ACK sip:' + SERVER_NAME + '@' + SERVER_IP + ' SIP/2.0\r\n'
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER_IP, SERVER_PORT))
+if __name__ == '__main__':
+    try:
+        CONFIG = sys.argv[1]
+        METHOD = sys.argv[2]
+        OPTION = sys.argv[3]
+    except IndexError:
+        sys.exit("Usage: python3 uaclient.py config method option")
 
-    print("Enviando:\r\n" + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
+    parser = make_parser()
+    xHandler = XMLHandler()
+    parser.setContentHandler(xHandler)
+    parser.parse(open(CONFIG))
 
-    print('Recibido --\r\n', data.decode('utf-8'))
-    if data.decode('utf-8').split(' ')[1] == '100':
-        print("Enviando: " + ACK_LINE)
-        my_socket.send(bytes(ACK_LINE, 'utf-8') + b'\r\n')
+    configlist = xHandler.get_tags()
 
-    print("Terminando socket...")
+    writelog('Starting...', )
 
-print("Fin.")
+    SERVER_NAME = address.split('@')[0]
+    SERVER_IP = address.split('@')[1].split(':')[0]
+    SERVER_PORT = int(address.split('@')[1].split(':')[1])
+    SDP = '\r\nv=0\r\no=robin@gotham.com 127.0.0.1\r\n'
+    SDP += 's=misesion\r\nt=0\r\nm=audio 34543 RTP\r\n'
+    content_length = 'Content-Length: ' + str(len(SDP)) + '\r\n'
+    # Contenido que vamos a enviar
+    if METHOD == 'INVITE':
+        LINE = 'INVITE sip:' + SERVER_NAME + '@' + SERVER_IP + ' SIP/2.0\r\n'
+        LINE += content_length + SDP
+    elif METHOD == 'BYE':
+        LINE = 'BYE sip:' + SERVER_NAME + '@' + SERVER_IP + ' SIP/2.0\r\n'
+
+    ACK_LINE = 'ACK sip:' + SERVER_NAME + '@' + SERVER_IP + ' SIP/2.0\r\n'
+
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.connect((SERVER_IP, SERVER_PORT))
+
+        print("Enviando:\r\n" + LINE)
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
+
+        print('Recibido --\r\n', data.decode('utf-8'))
+        if data.decode('utf-8').split(' ')[1] == '100':
+            print("Enviando: " + ACK_LINE)
+            my_socket.send(bytes(ACK_LINE, 'utf-8') + b'\r\n')
+
+        print("Terminando socket...")
+
+    print("Fin.")
